58df0a325606e293af93657d2c4afd325d863d3e
Mouloud <merbouchemouloud@gmail.com>
2024-05-03 09:20:31 +0200

    Update organiserEVENT.php

--- htmls/organiserEVENT.php
+++ htmls/organiserEVENT.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page d'acceuil</title>
+    <title>Organiser un évènement</title>
     <link rel="stylesheet" href="../css/base.css">
     <link rel="stylesheet" href="../css/headerv2.css">
     <link rel="stylesheet" href="../css/footer.css">
