87d85f2a3300f81fecfa2dab6883aea1ee575630
Mouloud <merbouchemouloud@gmail.com>
2024-05-08 18:58:00 +0200

    ajout base de donnee
    fonctionel

--- mydb (14).sql
+++ mydb (14).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : jeu. 02 mai 2024 à 21:45
+-- Généré le : mer. 08 mai 2024 à 18:57
 -- Version du serveur : 10.4.28-MariaDB
 -- Version de PHP : 8.2.4
 
@@ -80,21 +80,15 @@ CREATE TABLE `Concert` (
   `Categorie` varchar(45) DEFAULT NULL,
   `Etat` varchar(45) DEFAULT NULL,
   `Like` int(11) DEFAULT NULL,
-  `idConcert_donnees` int(11) NOT NULL
+  `ImagePath` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Structure de la table `Concert_donnees`
+-- Déchargement des données de la table `Concert`
 --
 
-CREATE TABLE `Concert_donnees` (
-  `idConcert_donnees` int(11) NOT NULL,
-  `Niveau_sonore_min` int(11) DEFAULT NULL,
-  `Niveau_sonore_moy` int(11) DEFAULT NULL,
-  `Niveau_sonore_max` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+INSERT INTO `Concert` (`idConcert`, `Titre`, `Date_concert`, `Duree`, `Description`, `Categorie`, `Etat`, `Like`, `ImagePath`) VALUES
+(21, 'Beatlssss', '2024-05-03', NULL, 'Concert en salle fermée ! Maximu 2000 personnes', NULL, NULL, NULL, 'concert1.jpg');
 
 -- --------------------------------------------------------
 
@@ -106,6 +100,45 @@ CREATE TABLE `Concert_has_Artiste` (
   `idConcert` int(11) NOT NULL,
   `idArtiste` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Donnees`
+--
+
+CREATE TABLE `Donnees` (
+  `idDonnees` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `niveauSonore` int(11) NOT NULL,
+  `idCapteurSonore` int(11) NOT NULL,
+  `Heure` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Déchargement des données de la table `Donnees`
+--
+
+INSERT INTO `Donnees` (`idDonnees`, `date`, `niveauSonore`, `idCapteurSonore`, `Heure`) VALUES
+(89, '2024-05-02', 65, 1, '2024-05-02 08:00:00'),
+(90, '2024-05-02', 70, 1, '2024-05-02 09:00:00'),
+(91, '2024-05-02', 75, 1, '2024-05-02 10:00:00'),
+(95, '2024-05-01', 60, 1, '2024-05-01 00:00:00'),
+(96, '2024-05-01', 62, 1, '2024-05-01 01:00:00'),
+(97, '2024-05-01', 64, 1, '2024-05-01 02:00:00'),
+(98, '2024-05-01', 63, 1, '2024-05-01 03:00:00'),
+(99, '2024-05-01', 65, 1, '2024-05-01 04:00:00'),
+(100, '2024-05-01', 67, 1, '2024-05-01 05:00:00'),
+(101, '2024-05-02', 68, 1, '2024-05-02 00:00:00'),
+(102, '2024-05-02', 70, 1, '2024-05-02 01:00:00'),
+(103, '2024-05-03', 72, 1, '2024-05-03 00:00:00'),
+(104, '2024-05-04', 73, 1, '2024-05-04 00:00:00'),
+(105, '2024-05-05', 74, 1, '2024-05-05 00:00:00'),
+(106, '2024-05-06', 75, 1, '2024-05-06 00:00:00'),
+(107, '2024-05-07', 76, 1, '2024-05-07 00:00:00'),
+(108, '2024-05-08', 77, 1, '2024-05-08 00:00:00'),
+(109, '2024-05-09', 78, 1, '2024-05-09 00:00:00'),
+(110, '2024-05-10', 79, 1, '2024-05-10 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -154,8 +187,8 @@ CREATE TABLE `Utilisateur` (
 
 INSERT INTO `Utilisateur` (`idUtilisateur`, `Nom`, `Prenom`, `Mail`, `Mdp`, `Type`, `Numero`, `Date_inscription`, `isAdmin`) VALUES
 (52, 'Admin', 'Admin', 'admin@audioinsight.com', '$2y$10$syc./7B.7LRQXjgc2bLEDOHqDnJCDEppNn09nESF6pfpMble5ZV3O', NULL, NULL, NULL, 1),
-(77, 'Inscription', 'Inscription', 'r@t.com', '$2y$10$7taXhBYh.tgaAy.yfswrBOnoxrtOEbhW9FfoZDciKwYJDp.kPlq5y', NULL, '0616892080', '2024-05-02', 0),
-(80, 'r', 'jean', 'jean@mail.com', '$2y$10$cc4nuyLntwwXa/oBc/BY9uCStGDVe2lFI0PNc7I3waAycOVqnOctC', NULL, NULL, NULL, 0);
+(81, 'ee', 'ee', 'r@t.comaa', '$2y$10$oyS8QDwpSTrwA0OC/ZOktOSZQxlEnRdGCG8GoqydcGWK6WNgz4VOS', NULL, '0616892081', '2024-05-03', 0),
+(84, 's', 'a', 'r@t.com', '$2y$10$of5Wk5hK80vD6WZcfBeMMezJa5nQzlpERcimkAfIpKq13vZvuvhIu', NULL, NULL, NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -196,14 +229,7 @@ ALTER TABLE `Commentaire`
 -- Index pour la table `Concert`
 --
 ALTER TABLE `Concert`
-  ADD PRIMARY KEY (`idConcert`),
-  ADD KEY `idx_Concert_idConcert_donnees` (`idConcert_donnees`);
-
---
--- Index pour la table `Concert_donnees`
---
-ALTER TABLE `Concert_donnees`
-  ADD PRIMARY KEY (`idConcert_donnees`);
+  ADD PRIMARY KEY (`idConcert`);
 
 --
 -- Index pour la table `Concert_has_Artiste`
@@ -211,6 +237,13 @@ ALTER TABLE `Concert_donnees`
 ALTER TABLE `Concert_has_Artiste`
   ADD PRIMARY KEY (`idConcert`),
   ADD KEY `idx_Concert_has_Artiste_idArtiste` (`idArtiste`);
+
+--
+-- Index pour la table `Donnees`
+--
+ALTER TABLE `Donnees`
+  ADD PRIMARY KEY (`idDonnees`),
+  ADD KEY `idCapteurSonore` (`idCapteurSonore`);
 
 --
 -- Index pour la table `FAQ`
@@ -243,6 +276,18 @@ ALTER TABLE `Billet_achete`
   MODIFY `idBillet` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
+-- AUTO_INCREMENT pour la table `Concert`
+--
+ALTER TABLE `Concert`
+  MODIFY `idConcert` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT pour la table `Donnees`
+--
+ALTER TABLE `Donnees`
+  MODIFY `idDonnees` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=111;
+
+--
 -- AUTO_INCREMENT pour la table `FAQ`
 --
 ALTER TABLE `FAQ`
@@ -252,17 +297,11 @@ ALTER TABLE `FAQ`
 -- AUTO_INCREMENT pour la table `Utilisateur`
 --
 ALTER TABLE `Utilisateur`
-  MODIFY `idUtilisateur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
+  MODIFY `idUtilisateur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=85;
 
 --
 -- Contraintes pour les tables déchargées
 --
-
---
--- Contraintes pour la table `Concert`
---
-ALTER TABLE `Concert`
-  ADD CONSTRAINT `fk_Concert_Concert_donnees` FOREIGN KEY (`idConcert_donnees`) REFERENCES `Concert_donnees` (`idConcert_donnees`);
 
 --
 -- Contraintes pour la table `Concert_has_Artiste`
