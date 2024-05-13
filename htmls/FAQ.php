fad8858fa85e397f07b959db65e51089124e92c7
baptiste146970 <baptiste.cihuelo@gmail.com>
2024-05-13 14:31:09 +0200

    formulaire marche bien avec BDD

--- htmls/FAQ.php
+++ htmls/FAQ.php
@@ -1,15 +1,36 @@
 <?php
+session_start(); // Doit être la première ligne avant les balises HTML
 $servername = "localhost"; // L'adresse du serveur de base de données
 $username = "root"; // Le nom d'utilisateur pour se connecter à la base de données
 $password = "root"; // Le mot de passe pour se connecter à la base de données
-$dbname = "mydb3"; // Le nom de la base de données
+$dbname = "mydb4"; // Le nom de la base de données
 
 // Créer une connexion
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Vérifier la connexion
 if ($conn->connect_error) {
+    echo "ssqfqs";
     die("Connection failed: " . $conn->connect_error);
 }else{
 }
+
+// Vérifier si le formulaire a été soumis
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Récupérer les données du formulaire
+    $question = $_POST["Question"];
+    $answer = $_POST["Réponse"];
+
+    // Préparer la requête SQL
+    $sql = "INSERT INTO  faq ( titre, contenu) VALUES ('$question', '$answer')";
+
+    // Exécuter la requête
+    if ($conn->query($sql) === TRUE) {
+        echo "Question ajoutée avec succès.";
+    } else {
+        echo "Erreur : " . $sql . "<br>" . $conn->error;
+    }
+}
+
+
 ?>
