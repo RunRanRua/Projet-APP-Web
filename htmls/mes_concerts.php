e01a07cac76b3f084d9a41cb900f486a73baf99c
Booboo123478 <marcantoinetissot7@gmail.com>
2024-06-05 20:10:39 +0200

    .

--- htmls/mes_concerts.php
+++ htmls/mes_concerts.php
@@ -125,7 +125,6 @@
 
 
 
-
     <!-- === Footer Part === -->
     <div class="footer">
 
--- php/get_billets_achetes.php
+++ php/get_billets_achetes.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
@@ -34,4 +34,5 @@ if ($resultBillets->num_rows > 0) {
 } else {
     echo "<p>Aucun billet acheté trouvé.</p>";
 }
+*/
 ?>
