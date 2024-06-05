12d58c99b02298d59e45a8ebd84d92c819914144
Booboo123478 <marcantoinetissot7@gmail.com>
2024-06-05 12:00:45 +0200

    profil logo update

--- htmls/profil.php
+++ htmls/profil.php
@@ -44,10 +44,10 @@
             <?php if ( $_SESSION["is_admin"] ) { ?>
                 <a href="#" > <img src="..\images\admin_icon.png" alt="Profil"> </a>
             <?php } else{ ?>
-                <a href="profil.php" > <img src="..\images\user.png" alt="Profil"> </a>
+                <a href="profil.php" > <img src="..\images\user.png" id="logoprofil" alt="Profil"> </a>
             <?php } ?>
 
-            <a href="..\php\logout.php"> <img src="..\images\Logout.png" alt="Déconnexion"> </a>
+            <a href="..\php\logout.php"> <img src="..\images\Logout.png" id="logologout" alt="Déconnexion"> </a>
             
         </div>
     </div>
