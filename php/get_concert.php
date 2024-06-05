094b2ef9b5f5936e1105ceb899d2c96351c2dd1f
Booboo123478 <marcantoinetissot7@gmail.com>
2024-06-05 19:32:17 +0200

    .

--- php/get_concert.php
+++ php/get_concert.php
@@ -4,6 +4,17 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mydb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 $userId = $_SESSION['user_id'];
 
